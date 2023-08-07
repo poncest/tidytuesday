4a898c39b220060eea247e3177275cd46011f01e
poncest <steven_ponce@yahoo.com>
2023-08-07 18:20:52 -0400

    week 32

--- 2023/Week_32/2023_32.R
+++ 2023/Week_32/2023_32.R
@@ -89,7 +89,6 @@ sauces_df <- sauces %>%
 
 
 
-
 # 5. VISUALIZATION ---- 
 
 ### |- plot aesthetics ---- 
@@ -107,7 +106,6 @@ col_palette <- list("Mild"        = "#46c8fe",
                     "Really Hot"  = "#cb4b94",
                     "Fiery Hot"   = "#d7418b", 
                     "Extreme Hot" = "#fb206f")
-
 
 
 ### |-  titles and caption ----
@@ -234,22 +232,21 @@ sauces_df %>%
   
 
 sessioninfo::session_info(include_base = TRUE) 
-## 6. SESSION INFO ---- 
-# ─ Session info ───────────────────────────────────────────────────────────────────────
+# ─ Session info ───────────────────────────────────────────────────────────
 # setting  value
 # version  R version 4.3.1 (2023-06-16 ucrt)
-# os       Windows 10 x64 (build 19044)
+# os       Windows 10 x64 (build 19045)
 # system   x86_64, mingw32
 # ui       RStudio
 # language (EN)
 # collate  English_United States.utf8
 # ctype    English_United States.utf8
 # tz       America/New_York
-# date     2023-08-01
+# date     2023-08-07
 # rstudio  2023.06.1+524 Mountain Hydrangea (desktop)
 # pandoc   NA
 # 
-# ─ Packages ───────────────────────────────────────────────────────────────────────────
+# ─ Packages ───────────────────────────────────────────────────────────────
 # package      * version date (UTC) lib source
 # base         * 4.3.1   2023-06-16 [2] local
 # bit            4.0.5   2022-11-15 [1] CRAN (R 4.3.0)
@@ -263,50 +260,38 @@ sessioninfo::session_info(include_base = TRUE)
 # crayon         1.5.2   2022-09-29 [1] CRAN (R 4.3.0)
 # curl           5.0.0   2023-01-12 [1] CRAN (R 4.3.0)
 # datasets     * 4.3.1   2023-06-16 [2] local
-# digest         0.6.31  2022-12-11 [1] CRAN (R 4.3.0)
 # dplyr        * 1.1.2   2023-04-20 [1] CRAN (R 4.3.0)
 # fansi          1.0.4   2023-01-22 [1] CRAN (R 4.3.0)
 # farver         2.1.1   2022-07-06 [1] CRAN (R 4.3.0)
 # forcats      * 1.0.0   2023-01-29 [1] CRAN (R 4.3.0)
 # fs             1.6.2   2023-04-25 [1] CRAN (R 4.3.0)
 # generics       0.1.3   2022-07-05 [1] CRAN (R 4.3.0)
-# ggforce        0.4.1   2022-10-04 [1] CRAN (R 4.3.1)
 # ggplot2      * 3.4.2   2023-04-03 [1] CRAN (R 4.3.0)
-# ggraph       * 2.1.0   2022-10-09 [1] CRAN (R 4.3.1)
-# ggrepel        0.9.3   2023-02-03 [1] CRAN (R 4.3.1)
 # ggtext       * 0.1.2   2022-09-16 [1] CRAN (R 4.3.0)
 # gifski         1.12.0  2023-05-19 [1] CRAN (R 4.3.0)
 # glue           1.6.2   2022-02-24 [1] CRAN (R 4.3.0)
 # graphics     * 4.3.1   2023-06-16 [2] local
-# graphlayouts   1.0.0   2023-05-01 [1] CRAN (R 4.3.1)
 # grDevices    * 4.3.1   2023-06-16 [2] local
 # grid           4.3.1   2023-06-16 [2] local
-# gridExtra      2.3     2017-09-09 [1] CRAN (R 4.3.0)
 # gridtext       0.1.5   2022-09-16 [1] CRAN (R 4.3.0)
 # gtable         0.3.3   2023-03-21 [1] CRAN (R 4.3.0)
 # here           1.0.1   2020-12-13 [1] CRAN (R 4.3.0)
 # hms            1.1.3   2023-03-21 [1] CRAN (R 4.3.0)
 # httr           1.4.6   2023-05-08 [1] CRAN (R 4.3.0)
-# igraph       * 1.4.3   2023-05-22 [1] CRAN (R 4.3.0)
-# janeaustenr    1.0.0   2022-08-26 [1] CRAN (R 4.3.0)
 # janitor      * 2.2.0   2023-02-02 [1] CRAN (R 4.3.0)
 # jsonlite       1.8.5   2023-06-05 [1] CRAN (R 4.3.0)
 # labeling       0.4.2   2020-10-20 [1] CRAN (R 4.3.0)
-# lattice        0.21-8  2023-04-05 [2] CRAN (R 4.3.1)
 # lifecycle      1.0.3   2022-10-07 [1] CRAN (R 4.3.0)
 # lubridate    * 1.9.2   2023-02-10 [1] CRAN (R 4.3.0)
 # magick         2.7.4   2023-03-09 [1] CRAN (R 4.3.0)
 # magrittr       2.0.3   2022-03-30 [1] CRAN (R 4.3.0)
 # markdown       1.7     2023-05-16 [1] CRAN (R 4.3.0)
-# MASS           7.3-60  2023-05-04 [2] CRAN (R 4.3.1)
-# Matrix         1.5-4.1 2023-05-18 [2] CRAN (R 4.3.1)
 # methods      * 4.3.1   2023-06-16 [2] local
 # munsell        0.5.0   2018-06-12 [1] CRAN (R 4.3.0)
 # pacman         0.5.1   2019-03-11 [1] CRAN (R 4.3.0)
 # parallel       4.3.1   2023-06-16 [2] local
 # pillar         1.9.0   2023-03-22 [1] CRAN (R 4.3.0)
 # pkgconfig      2.0.3   2019-09-22 [1] CRAN (R 4.3.0)
-# polyclip       1.10-4  2022-10-20 [1] CRAN (R 4.3.0)
 # purrr        * 1.0.1   2023-01-10 [1] CRAN (R 4.3.0)
 # R6             2.5.1   2021-08-19 [1] CRAN (R 4.3.0)
 # ragg           1.2.5   2023-01-12 [1] CRAN (R 4.3.0)
@@ -324,7 +309,6 @@ sessioninfo::session_info(include_base = TRUE)
 # showtext     * 0.9-6   2023-05-03 [1] CRAN (R 4.3.0)
 # showtextdb   * 3.0     2020-06-04 [1] CRAN (R 4.3.0)
 # snakecase      0.11.0  2019-05-25 [1] CRAN (R 4.3.0)
-# SnowballC      0.7.1   2023-04-25 [1] CRAN (R 4.3.0)
 # stats        * 4.3.1   2023-06-16 [2] local
 # stringi        1.7.12  2023-01-11 [1] CRAN (R 4.3.0)
 # stringr      * 1.5.0   2022-12-02 [1] CRAN (R 4.3.0)
@@ -333,23 +317,17 @@ sessioninfo::session_info(include_base = TRUE)
 # systemfonts    1.0.4   2022-02-11 [1] CRAN (R 4.3.0)
 # textshaping    0.3.6   2021-10-13 [1] CRAN (R 4.3.0)
 # tibble       * 3.2.1   2023-03-20 [1] CRAN (R 4.3.0)
-# tidygraph      1.2.3   2023-02-01 [1] CRAN (R 4.3.1)
 # tidyr        * 1.3.0   2023-01-24 [1] CRAN (R 4.3.0)
 # tidyselect     1.2.0   2022-10-10 [1] CRAN (R 4.3.0)
-# tidytext     * 0.4.1   2023-01-07 [1] CRAN (R 4.3.0)
 # tidytuesdayR * 1.0.2   2022-02-01 [1] CRAN (R 4.3.0)
 # tidyverse    * 2.0.0   2023-02-22 [1] CRAN (R 4.3.0)
 # timechange     0.2.0   2023-01-11 [1] CRAN (R 4.3.0)
-# tokenizers     0.3.0   2022-12-22 [1] CRAN (R 4.3.0)
 # tools          4.3.1   2023-06-16 [2] local
-# tweenr         2.0.2   2022-09-06 [1] CRAN (R 4.3.1)
 # tzdb           0.4.0   2023-05-12 [1] CRAN (R 4.3.0)
 # usethis        2.2.0   2023-06-06 [1] CRAN (R 4.3.0)
 # utf8           1.2.3   2023-01-31 [1] CRAN (R 4.3.0)
 # utils        * 4.3.1   2023-06-16 [2] local
 # vctrs          0.6.2   2023-04-19 [1] CRAN (R 4.3.0)
-# viridis        0.6.3   2023-05-03 [1] CRAN (R 4.3.1)
-# viridisLite    0.4.2   2023-05-02 [1] CRAN (R 4.3.0)
 # vroom          1.6.3   2023-04-28 [1] CRAN (R 4.3.0)
 # withr          2.5.0   2022-03-03 [1] CRAN (R 4.3.0)
 # xfun           0.39    2023-04-20 [1] CRAN (R 4.3.0)
@@ -358,7 +336,7 @@ sessioninfo::session_info(include_base = TRUE)
 # [1] C:/Users/poncest/AppData/Local/R/win-library/4.3
 # [2] C:/Users/poncest/AppData/Local/Programs/R/R-4.3.1/library
 # 
-# ──────────────────────────────────────────────────────────────────────────────────────
-# > 
+# ──────────────────────────────────────────────────────────────────────────
+# >
 
   
