d62b8bab4f95a272c151441e3eb713a0c47e9a91
poncest <steven_ponce@yahoo.com>
2024-10-07 17:05:37 -0400

    week 41

--- 2024/Week_41/2024_41.R
+++ 2024/Week_41/2024_41.R
@@ -1,12 +1,11 @@
 
-## Challenge: #TidyTuesday 2024 week 40
-## Data:      Chess Game Dataset (Lichess)
+## Challenge: #TidyTuesday 2024 week 41
+## Data:      National Park Species
 ## Author:    Steven Ponce
-## Date:      2024-09-25
+## Date:      2024-10-07
 
 ## Reference: 
 # https://pmassicotte.github.io/paletteer_gallery/
-# https://ggfx.data-imaginist.com/
 
 
 ## 1. LOAD PACKAGES & SETUP ----
@@ -19,15 +18,21 @@ pacman::p_load(
     scales,            # Scale Functions for Visualization
     lubridate,         # Make Dealing with Dates a Little Easier
     glue,              # Interpreted String Literals
-    ggfx               # Pixel Filters for 'ggplot2' and 'grid' # Pixel Filters for 'ggplot2' and 'grid' 
+    fmsb,              # Functions for Medical Statistics Book with some Demographic Data
+    purrr,             # Functional Programming Tools
+    patchwork,         # The Composer of Plots
+    grid,              # The Grid Graphics Package
+    cowplot,           # Streamlined Plot Theme and Plot Annotations for 'ggplot2'
+    png,               # Read and write PNG images
+    here               # A Simpler Way to Find Your Files
 )  
 
 ### |- figure size ---- 
 camcorder::gg_record(
     dir    = here::here("temp_plots"),
     device = "png",
-    width  =  7.5,
-    height =  5,
+    width  =  10,
+    height =  10,
     units  = "in",
     dpi    = 320
 )
@@ -37,9 +42,9 @@ showtext_opts(dpi = 320, regular.wt = 300, bold.wt = 800)
 
 
 ## 2. READ IN THE DATA ----
-tt <-tidytuesdayR::tt_load(2024, week = 40) 
+tt <-tidytuesdayR::tt_load(2024, week = 41) 
 
-chess <- tt$chess |> clean_names() |> glimpse()
+species_data <- tt$most_visited_nps_species_data |> clean_names() |> glimpse()
 
 tidytuesdayR::readme(tt)
 rm(tt)
@@ -47,29 +52,21 @@ rm(tt)
 
 
 ## 3. EXAMINING THE DATA ----
-glimpse(chess)
-skim(chess)
+glimpse(species_data)
+skim(species_data)
 
 
 
 ## 4. TIDYDATA ----
 
-# Tidy
-chess_data <- chess |>  
-    select(white_rating, black_rating) |>
-    pivot_longer(
-        cols = c(white_rating, black_rating), 
-        names_to = "player", 
-        values_to = "rating") |>
-    mutate(player = ifelse(player == "white_rating", "White", "Black"))
-
-# Labels 
-labels <- tibble(
-    label = c("White", "Black"),
-    player = c("White", "Black"),
-    x = c(1600, 1600), 
-    y = c(0.0001, 0.0001) 
-)
+# Prepare data for radar plots
+radar_data <- species_data |>
+  count(park_name, category_name) |>
+  pivot_wider(
+    names_from = category_name,
+    values_from = n,
+    values_fill = 0
+  )
 
 
 
@@ -81,18 +78,18 @@ title_col    <- "gray20"
 subtitle_col <- "gray20"     
 caption_col  <- "gray30"   
 text_col     <- "gray20"    
-col_palette  <- paletteer::paletteer_d("peRReo::don")[c(5,9)]
+col_palette  <- paletteer::paletteer_d("ButterflyColors::fountainea_ryphea")[c(1)] 
 
 ### |-  titles and caption ----
 # icons
-tt <- str_glue("#TidyTuesday: { 2024 } Week { 40 } &bull; Source: Chess Game Dataset (Lichess)<br>")
+tt <- str_glue("#TidyTuesday: { 2024 } Week { 41 } &bull; Source: NPSpecies - The National Park Service biodiversity database<br>")
 li <- str_glue("<span style='font-family:fa6-brands'>&#xf08c;</span>")
 gh <- str_glue("<span style='font-family:fa6-brands'>&#xf09b;</span>")
 mn <- str_glue("<span style='font-family:fa6-brands'>&#xf4f6;</span>")
 
 # text
-title_text    <- str_glue("White and Black Chess Ratings: A Distribution Analysis")
-subtitle_text <- str_glue("How ratings vary between White and Black players across competitive chess matches")
+title_text    <- str_glue("Radar Charts of Species Categories by National Park")
+subtitle_text <- str_glue("Comparison of species distributions across U.S. national parks")
 caption_text  <- str_glue("{tt} {li} stevenponce &bull; {mn} @sponce1(graphic.social) {gh} poncest &bull; #rstats #ggplot2")
 
 ### |-  fonts ----
@@ -104,7 +101,7 @@ font_add_google("Noto Sans", regular.wt = 400, family = "caption")
 showtext_auto(enable = TRUE)
 
 ### |-  plot theme ----
-theme_set(theme_minimal(base_size = 14, base_family = "text"))                
+theme_set(theme_minimal(base_size = 12, base_family = "text"))                
 
 theme_update(
     plot.title.position   = "plot",
@@ -119,90 +116,114 @@ theme_update(
     axis.title.y          = element_blank(),  
     axis.text.y           = element_blank(),
     axis.text.x           = element_text(color = text_col, family = "text", size = rel(0.9)),
-    axis.ticks.x          = element_line(color = text_col),  # Show x-axis ticks
-    panel.grid            = element_blank(),
-    strip.text            = element_blank() # Remove strip text 
+    axis.ticks.x          = element_line(color = text_col),  
 )
 
-### |-  final plot ----
-ggplot(chess_data, aes(x = rating)) +
+### |- plot function ----
+
+# Function to create radar chart and save as PNG
+create_and_save_radar_plot <- function(data, park_name) {
     
-    # Geoms
-    # Reference text layer
-    as_reference(
-        geom_text(
-            data = labels, aes(x = x, y = y, label = label),
-            family = "title", colour = "gray10", size = rel(35), hjust = 0.5, vjust = 0
-        ),
-        id = "text"
-    ) +
+    # Define maximum and minimum values for the radar chart
+    max_values <- rep(max(data), ncol(data))
+    min_values <- rep(0, ncol(data))
     
-    # Blending the text with the density plot
-    with_inner_glow(
-        with_blend(
-            geom_density(aes(fill = player, color = player),
-                         alpha = 1, show.legend = FALSE,
-                         bw = 25,
-                         kernel = "epanechnikov"
-            ),
-            bg_layer = "text", blend_type = "xor"
-        ),
-        color = "gray10", sigma = 15
-    ) +
+    # Combine the max, min, and park data to create the radar chart data frame
+    plot_data <- as.data.frame(rbind(max_values, min_values, data))
+    colnames(plot_data) <- names(data)
+    rownames(plot_data) <- c("Max", "Min", park_name)
     
-    # Labs
-    labs(
-        x = "Player Rating",
-        y = NULL,
+    # Define the file path to save the radar chart
+    file_path <- here("2024/Week_41/", paste0("radar_plot_", gsub(" ", "_", park_name), ".png"))
+    
+    # Create and save the radar chart as a PNG
+    png(filename = file_path, width = 400, height = 400)
+    fmsb::radarchart(plot_data,
+                     axistype = 1,                             # Axis type configuration
+                     title = park_name,                        # Title for the radar chart
+                     pcol = col_palette,                       # Line color for the polygon
+                     pfcol = scales::alpha(col_palette, 0.25), # Fill color for the polygon with transparency
+                     plty = 1,                                 # Line type for the polygon
+                     cglcol = bkg_col,                         # Color of the grid lines
+                     cglty = 1,                                # Type of the grid lines
+                     cglwd = 0.8,                              # Width of the grid lines
+                     axislabcol = bkg_col,                     # Color of the axis labels
+                     cex.axis = 1.2,                           # Increase axis text size
+                     cex.main = 1.5                            # Increase title text size
+    )
+    dev.off()
+    
+    return(file_path)
+}
+
+### |-  individual plots ----
+# Generate and save radar plots for each park
+showtext_auto(enable = FALSE)
+radar_plot_files <- radar_data |>
+    split(radar_data$park_name) |>
+    map_chr(~ {
+        park_name <- .x$park_name[1]                      # Extract park name
+        park_data <- .x |> select(-park_name)             # Remove park name column from data
+        create_and_save_radar_plot(park_data, park_name)  # Create and save radar plot
+    })
+
+
+# Load each saved radar plot as a raster image and convert to ggplot
+radar_plots <- map(radar_plot_files, ~ {
+    img <- readPNG(.x)                                    # Read the saved PNG file
+    ggdraw() + 
+        draw_image(img)                                   # Convert the image to a ggplot object
+    })
+
+
+### |-  combined plots ----
+showtext_auto(enable = TRUE)
+
+combined_plot <- wrap_plots(radar_plots, ncol = 4) +
+    plot_annotation(
+        
+        # Labs
         title = title_text,
-        subtitle = subtitle_text,
-        caption = caption_text
-    ) +
-    
-    # Scales
-    scale_x_continuous() +
-    scale_y_continuous() +
-    scale_fill_manual(values = col_palette) +
-    scale_color_manual(values = col_palette) +
-    coord_cartesian(clip = "off") +
-    
-    # Facet
-    facet_wrap(~player, ncol = 1) +
-    
-    # Theme
-    theme(
-        plot.title = element_text(
-            size = rel(1.55),
-            family = "title",
-            face = "bold",
-            color = title_col,
-            lineheight = 1.1,
-            margin = margin(t = 5, b = 5)
-        ),
-        plot.subtitle = element_text(
-            size = rel(0.85),
-            family = "subtitle",
-            color = subtitle_col,
-            lineheight = 1.1,
-            margin = margin(t = 5, b = 5)
-        ),
-        plot.caption = element_markdown(
-            size = rel(0.50),
-            family = "caption",
-            color = caption_col,
-            lineheight = 1.1,
-            hjust = 0.5,
-            halign = 1,
-            margin = margin(t = 5, b = 5)
+        subtitle = title_text,
+        caption = caption_text,
+        
+        # Theme
+        theme = theme(
+            plot.title = element_text(
+                size = rel(1.7),
+                family = "title",
+                face = "bold",
+                color = title_col,
+                lineheight = 1.1,
+                margin = margin(t = 5, b = 5)
+            ),
+            plot.subtitle = element_text(
+                size = rel(1),
+                family = "subtitle",
+                color = subtitle_col,
+                lineheight = 1.1,
+                margin = margin(t = 5, b = 5)
+            ),
+            plot.caption = element_markdown(
+                size = rel(0.65),
+                family = "caption",
+                color = caption_col,
+                lineheight = 1.1,
+                hjust = 0.5,
+                halign = 1,
+                margin = margin(t = 5, b = 5)
+            )
         )
     )
+
+combined_plot 
 
 
 
 # 6. SESSION INFO ---- 
 sessioninfo::session_info(include_base = TRUE)
 
-# ─ Session info ─────────────────────────────────────────────────────────────────────────────
+# ─ Session info ────────────────────────────────────────────────────────────────────
 # setting  value
 # version  R version 4.4.1 (2024-06-14 ucrt)
 # os       Windows 10 x64 (build 19045)
@@ -212,11 +233,11 @@ sessioninfo::session_info(include_base = TRUE)
 # collate  English_United States.utf8
 # ctype    English_United States.utf8
 # tz       America/New_York
-# date     2024-09-24
-# rstudio  2024.04.2+764 Chocolate Cosmos (desktop)
+# date     2024-10-07
+# rstudio  2024.09.0+375 Cranberry Hibiscus (desktop)
 # pandoc   NA
 # 
-# ─ Packages ─────────────────────────────────────────────────────────────────────────────────
+# ─ Packages ────────────────────────────────────────────────────────────────────────
 # ! package      * version  date (UTC) lib source
 # V base         * 4.4.1    2024-04-24 [2] local (on disk 4.4.0)
 # P base64enc      0.1-3    2015-07-28 [?] CRAN (R 4.4.0)
@@ -228,6 +249,7 @@ sessioninfo::session_info(include_base = TRUE)
 # colorspace     2.1-0    2023-01-23 [1] CRAN (R 4.4.0)
 # P commonmark     1.9.1    2024-01-30 [?] CRAN (R 4.4.0)
 # P compiler       4.4.0    2024-04-24 [?] local
+# cowplot      * 1.1.3    2024-01-22 [1] CRAN (R 4.4.0)
 # P crayon         1.5.2    2022-09-29 [?] CRAN (R 4.4.0)
 # P curl           5.2.1    2024-03-01 [?] CRAN (R 4.4.0)
 # P datasets     * 4.4.0    2024-04-24 [?] local
@@ -236,27 +258,26 @@ sessioninfo::session_info(include_base = TRUE)
 # P fansi          1.0.6    2023-12-08 [?] CRAN (R 4.4.0)
 # farver         2.1.2    2024-05-13 [1] CRAN (R 4.4.1)
 # P fastmap        1.1.1    2023-02-24 [?] CRAN (R 4.4.0)
+# P fmsb         * 0.7.6    2024-01-19 [?] CRAN (R 4.4.0)
 # forcats      * 1.0.0    2023-01-29 [1] CRAN (R 4.4.0)
 # P fs             1.6.4    2024-04-25 [?] CRAN (R 4.4.0)
 # generics       0.1.3    2022-07-05 [1] CRAN (R 4.4.0)
-# P ggfx         * 1.0.1    2022-08-22 [?] CRAN (R 4.4.0)
 # ggplot2      * 3.5.1    2024-04-23 [1] CRAN (R 4.4.0)
 # P ggtext       * 0.1.2    2022-09-16 [?] CRAN (R 4.4.0)
 # P gifski         1.12.0-2 2023-08-12 [?] CRAN (R 4.4.0)
 # P glue         * 1.7.0    2024-01-09 [?] CRAN (R 4.4.0)
 # P graphics     * 4.4.0    2024-04-24 [?] local
 # P grDevices    * 4.4.0    2024-04-24 [?] local
-# P grid           4.4.0    2024-04-24 [?] local
+# P grid         * 4.4.0    2024-04-24 [?] local
 # P gridtext       0.1.5    2022-09-16 [?] CRAN (R 4.4.0)
 # gtable         0.3.5    2024-04-22 [1] CRAN (R 4.4.0)
-# P here           1.0.1    2020-12-13 [?] CRAN (R 4.4.0)
+# P here         * 1.0.1    2020-12-13 [?] CRAN (R 4.4.0)
 # P hms            1.1.3    2023-03-21 [?] CRAN (R 4.4.0)
 # P htmltools      0.5.8.1  2024-04-04 [?] CRAN (R 4.4.0)
 # P httr           1.4.7    2023-08-15 [?] CRAN (R 4.4.0)
 # P janitor      * 2.2.0    2023-02-02 [?] CRAN (R 4.4.0)
 # P jsonlite       1.8.8    2023-12-04 [?] CRAN (R 4.4.0)
 # P knitr          1.46     2024-04-06 [?] CRAN (R 4.4.0)
-# labeling       0.4.3    2023-08-29 [1] CRAN (R 4.4.0)
 # P lifecycle      1.0.4    2023-11-07 [?] CRAN (R 4.4.0)
 # P lubridate    * 1.9.3    2023-09-27 [?] CRAN (R 4.4.0)
 # magick         2.8.3    2024-02-18 [1] CRAN (R 4.4.0)
@@ -267,8 +288,10 @@ sessioninfo::session_info(include_base = TRUE)
 # P pacman         0.5.1    2019-03-11 [?] CRAN (R 4.4.0)
 # P paletteer      1.6.0    2024-01-21 [?] CRAN (R 4.4.0)
 # P parallel       4.4.0    2024-04-24 [?] local
+# P patchwork    * 1.2.0    2024-01-08 [?] CRAN (R 4.4.0)
 # P pillar         1.9.0    2023-03-22 [?] CRAN (R 4.4.0)
 # P pkgconfig      2.0.3    2019-09-22 [?] CRAN (R 4.4.0)
+# P png          * 0.1-8    2022-11-29 [?] CRAN (R 4.4.0)
 # P prismatic      1.1.2    2024-04-10 [?] CRAN (R 4.4.0)
 # P purrr        * 1.0.2    2023-08-10 [?] CRAN (R 4.4.0)
 # P R6             2.5.1    2021-08-19 [?] CRAN (R 4.4.0)
@@ -285,6 +308,7 @@ sessioninfo::session_info(include_base = TRUE)
 # P rsvg           2.6.0    2023-10-08 [?] CRAN (R 4.4.0)
 # P rvest          1.0.4    2024-02-12 [?] CRAN (R 4.4.0)
 # scales       * 1.3.0    2023-11-28 [1] CRAN (R 4.4.0)
+# P selectr        0.4-2    2019-11-20 [?] CRAN (R 4.4.0)
 # P sessioninfo    1.2.2    2021-12-06 [?] CRAN (R 4.4.0)
 # P showtext     * 0.9-7    2024-03-02 [?] CRAN (R 4.4.0)
 # P showtextdb   * 3.0      2020-06-04 [?] CRAN (R 4.4.0)
@@ -317,5 +341,5 @@ sessioninfo::session_info(include_base = TRUE)
 # V ── Loaded and on-disk version mismatch.
 # P ── Loaded and on-disk path mismatch.
 # 
-# ────────────────────────────────────────────────────────────────────────────────────────────
+# ───────────────────────────────────────────────────────────────────────────────────
 # > 
