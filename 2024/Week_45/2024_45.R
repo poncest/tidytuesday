c41db543459812342ace2a8754cdf0fb34887654
poncest <steven_ponce@yahoo.com>
2024-11-03 13:42:37 -0500

    week 45

---
+++ 2024/Week_45/2024_45.R
@@ -0,0 +1,305 @@
+## Challenge: #TidyTuesday 2024 week 45
+## Data:      Democracy and Dictatorship
+## Author:    Steven Ponce
+## Date:      2024-11-03
+
+## Reference: 
+# https://pmassicotte.github.io/paletteer_gallery/
+
+
+## 1. LOAD PACKAGES & SETUP ----
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+    tidyverse,         # Easily Install and Load the 'Tidyverse'
+    ggtext,            # Improved Text Rendering Support for 'ggplot2'
+    showtext,          # Using Fonts More Easily in R Graphs
+    janitor,           # Simple Tools for Examining and Cleaning Dirty Data
+    scales,            # Scale Functions for Visualization
+    glue,              # Interpreted String Literals
+    here,              # A Simpler Way to Find Your Files
+    geomtextpath       # Curved Text in 'ggplot2'
+)    
+
+### |- figure size ----
+camcorder::gg_record(
+    dir    = here::here("temp_plots"),
+    device = "png",
+    width  =  10,
+    height =  8,
+    units  = "in",
+    dpi    = 320
+)
+
+### |- resolution ----
+showtext_opts(dpi = 320, regular.wt = 300, bold.wt = 800)
+
+
+## 2. READ IN THE DATA ----
+tt <-tidytuesdayR::tt_load(2024, week = 45) 
+
+democracy_data <- tt$democracy_data |> clean_names() |> glimpse()
+
+tidytuesdayR::readme(tt)
+rm(tt)
+
+
+
+## 3. EXAMINING THE DATA ----
+glimpse(democracy_data)
+
+
+
+## 4. TIDYDATA ----
+
+### |- tidy data ----
+
+# Line Plot of Democracy Count Over Time 
+line_data <- democracy_data |>
+    group_by(year) |>
+    summarize(
+        democratic_count = sum(is_democracy, na.rm = TRUE),
+        non_democratic_count = sum(is_communist, na.rm = TRUE)
+    ) |>
+    ungroup()
+
+
+
+# 5. VISUALIZATION ----
+### |- plot aesthetics ----
+bkg_col      <- "#f5f5f2"  
+title_col    <- "gray20"           
+subtitle_col <- "gray20"     
+caption_col  <- "gray30"   
+text_col     <- "gray20"    
+
+### |-  titles and caption ----
+# icons
+tt <- str_glue("#TidyTuesday: { 2024 } Week { 45 } &bull; Source: democracyData R Package<br>")
+li <- str_glue("<span style='font-family:fa6-brands'>&#xf08c;</span>")
+gh <- str_glue("<span style='font-family:fa6-brands'>&#xf09b;</span>")
+bs <- str_glue("<span style='font-family:fa6-brands'>&#xe671; </span>")
+
+# text
+title_text    <- str_glue("Rise and Fall of Democracies and Non-Democracies (1950-2020)")
+subtitle_text <- str_wrap("Historical trend in the number of democratic versus non-democratic countries, 
+                          illustrating the growth of democracies and the decline of non-democracies over time",
+                          width = 85)
+caption_text  <- str_glue("{tt} {li} stevenponce &bull; {bs} sponce1 &bull; {gh} poncest &bull; #rstats #ggplot2")
+
+### |-  fonts ----
+font_add("fa6-brands", "fonts/6.6.0/Font Awesome 6 Brands-Regular-400.otf")
+font_add_google("Oswald", regular.wt = 400, family = "title")
+font_add_google("Merriweather Sans", regular.wt = 400, family = "subtitle")
+font_add_google("Merriweather Sans", regular.wt = 400, family = "text")
+font_add_google("Noto Sans", regular.wt = 400, family = "caption")
+showtext_auto(enable = TRUE)
+
+### |-  plot theme ----
+theme_set(theme_minimal(base_size = 14, base_family = "text"))                
+
+theme_update(
+    plot.title.position   = "plot",
+    plot.caption.position = "plot",
+    legend.position       = "plot",
+    plot.background       = element_rect(fill = bkg_col, color = bkg_col),
+    panel.background      = element_rect(fill = bkg_col, color = bkg_col),
+    plot.margin           = margin(t = 10, r = 20, b = 10, l = 20),
+    axis.title.x          = element_text(margin = margin(10, 0, 0, 0), size = rel(1.1), 
+                                         color = text_col, family = "text", face = "bold", hjust = 0.5),
+    axis.title.y          = element_text(margin = margin(0, 10, 0, 0), size = rel(1.1), 
+                                         color = text_col, family = "text", face = "bold", hjust = 0.5),
+    axis.text             = element_text(size = rel(0.8), color = text_col, family = "text"),
+    axis.ticks.x          = element_line(color = text_col),
+    axis.line.x           = element_line(color = "#252525", linewidth = .2),
+    panel.grid.minor      = element_blank(),
+    panel.grid.major      = element_blank(),
+    panel.grid.major.y    = element_line(color = "grey85", size = .4),
+)  
+
+### |- initial plot ----
+
+line_data |>
+  ggplot() +
+
+  # Geoms
+  geom_ribbon(aes(x = year, ymin = non_democratic_count, ymax = democratic_count), fill = "gray80", alpha = 0.5) +
+  geom_textline(
+    aes(x = year, y = democratic_count, label = "Democratic Count", color = "Democratic Count"),
+    linewidth = 1,
+    family = "text",
+    size = 4.5,
+    offset = unit(0.15, "cm"),
+    text_smoothing = 30
+  ) +
+  geom_textline(
+    aes(x = year, y = non_democratic_count, label = "Non-Democratic Count", color = "Non-Democratic Count"),
+    linewidth = 1,
+    family = "text",
+    size = 4.5,
+    offset = unit(0.15, "cm"),
+    text_smoothing = 30
+  ) +
+
+  # Scales
+  scale_x_continuous() +
+  scale_y_continuous(
+    breaks = seq(0, 150, by = 50),
+    limits = c(0, 150)
+  ) +
+  scale_color_manual(values = c(
+    "Democratic Count" = "#1B3A54FF",
+    "Non-Democratic Count" = "#913914FF"
+  )) +
+  coord_cartesian(clip = "off") +
+
+  # Labs
+  labs(
+    x = "Year",
+    y = "Count",
+    color = "Type",
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text
+  ) +
+
+  # Theme
+  theme(
+    axis.text.y = ggtext::element_markdown(),
+    plot.title = element_text(
+      size        = rel(1.75),
+      family      = "title",
+      face        = "bold",
+      color       = title_col,
+      margin      = margin(t = 5, b = 5)
+    ),
+    plot.subtitle = element_text(
+      size        = rel(1.1),
+      family      = "subtitle",
+      color       = title_col,
+      lineheight  = 1.1,
+      margin      = margin(t = 5, b = 15)
+    ),
+    plot.caption = element_markdown(
+      size        = rel(.65),
+      family      = "caption",
+      color       = caption_col,
+      lineheight  = 0.65,
+      hjust       = 0.5,
+      halign      = 0.5,
+      margin      = margin(t = 10, b = 5)
+    ),
+  )
+
+
+# 6. SESSION INFO ---- 
+sessioninfo::session_info(include_base = TRUE)
+
+# ─ Session info ────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.4.1 (2024-06-14 ucrt)
+# os       Windows 11 x64 (build 22631)
+# system   x86_64, mingw32
+# ui       RStudio
+# language (EN)
+# collate  English_United States.utf8
+# ctype    English_United States.utf8
+# tz       America/New_York
+# date     2024-11-03
+# rstudio  2024.09.0+375 Cranberry Hibiscus (desktop)
+# pandoc   NA
+# 
+# ─ Packages ────────────────────────────────────────────────────────────────────
+# ! package      * version  date (UTC) lib source
+# V base         * 4.4.1    2024-04-24 [2] local (on disk 4.4.0)
+# P bit            4.0.5    2022-11-15 [?] CRAN (R 4.4.0)
+# P bit64          4.0.5    2020-08-30 [?] CRAN (R 4.4.0)
+# P camcorder      0.1.0    2022-10-03 [?] CRAN (R 4.4.0)
+# P cellranger     1.1.0    2016-07-27 [?] CRAN (R 4.4.0)
+# cli            3.6.3    2024-06-21 [1] CRAN (R 4.4.1)
+# colorspace     2.1-0    2023-01-23 [1] CRAN (R 4.4.0)
+# P commonmark     1.9.1    2024-01-30 [?] CRAN (R 4.4.0)
+# P compiler       4.4.0    2024-04-24 [?] local
+# P crayon         1.5.2    2022-09-29 [?] CRAN (R 4.4.0)
+# P curl           5.2.1    2024-03-01 [?] CRAN (R 4.4.0)
+# P datasets     * 4.4.0    2024-04-24 [?] local
+# dplyr        * 1.1.4    2023-11-17 [1] CRAN (R 4.4.0)
+# P fansi          1.0.6    2023-12-08 [?] CRAN (R 4.4.0)
+# farver         2.1.2    2024-05-13 [1] CRAN (R 4.4.1)
+# forcats      * 1.0.0    2023-01-29 [1] CRAN (R 4.4.0)
+# P fs             1.6.4    2024-04-25 [?] CRAN (R 4.4.0)
+# generics       0.1.3    2022-07-05 [1] CRAN (R 4.4.0)
+# P geomtextpath * 0.1.4    2024-06-13 [?] CRAN (R 4.4.1)
+# ggplot2      * 3.5.1    2024-04-23 [1] CRAN (R 4.4.0)
+# P ggtext       * 0.1.2    2022-09-16 [?] CRAN (R 4.4.0)
+# P gifski         1.12.0-2 2023-08-12 [?] CRAN (R 4.4.0)
+# P glue         * 1.7.0    2024-01-09 [?] CRAN (R 4.4.0)
+# P graphics     * 4.4.0    2024-04-24 [?] local
+# P grDevices    * 4.4.0    2024-04-24 [?] local
+# P grid           4.4.0    2024-04-24 [?] local
+# P gridtext       0.1.5    2022-09-16 [?] CRAN (R 4.4.0)
+# gtable         0.3.5    2024-04-22 [1] CRAN (R 4.4.0)
+# P here         * 1.0.1    2020-12-13 [?] CRAN (R 4.4.0)
+# P hms            1.1.3    2023-03-21 [?] CRAN (R 4.4.0)
+# P httr           1.4.7    2023-08-15 [?] CRAN (R 4.4.0)
+# P janitor      * 2.2.0    2023-02-02 [?] CRAN (R 4.4.0)
+# P jsonlite       1.8.8    2023-12-04 [?] CRAN (R 4.4.0)
+# labeling       0.4.3    2023-08-29 [1] CRAN (R 4.4.0)
+# P lifecycle      1.0.4    2023-11-07 [?] CRAN (R 4.4.0)
+# P lubridate    * 1.9.3    2023-09-27 [?] CRAN (R 4.4.0)
+# magick         2.8.3    2024-02-18 [1] CRAN (R 4.4.0)
+# P magrittr       2.0.3    2022-03-30 [?] CRAN (R 4.4.0)
+# P markdown       1.12     2023-12-06 [?] CRAN (R 4.4.0)
+# P methods      * 4.4.0    2024-04-24 [?] local
+# munsell        0.5.1    2024-04-01 [1] CRAN (R 4.4.0)
+# P pacman       * 0.5.1    2019-03-11 [?] CRAN (R 4.4.0)
+# P parallel       4.4.0    2024-04-24 [?] local
+# P pillar         1.9.0    2023-03-22 [?] CRAN (R 4.4.0)
+# P pkgconfig      2.0.3    2019-09-22 [?] CRAN (R 4.4.0)
+# P purrr        * 1.0.2    2023-08-10 [?] CRAN (R 4.4.0)
+# P R6             2.5.1    2021-08-19 [?] CRAN (R 4.4.0)
+# P ragg           1.3.0    2024-03-13 [?] CRAN (R 4.4.0)
+# P Rcpp           1.0.12   2024-01-09 [?] CRAN (R 4.4.0)
+# P readr        * 2.1.5    2024-01-10 [?] CRAN (R 4.4.0)
+# P readxl         1.4.3    2023-07-06 [?] CRAN (R 4.4.0)
+# P renv           1.0.7    2024-04-11 [?] CRAN (R 4.4.0)
+# rlang          1.1.4    2024-06-04 [1] CRAN (R 4.4.1)
+# P rprojroot      2.0.4    2023-11-05 [?] CRAN (R 4.4.0)
+# P rstudioapi     0.16.0   2024-03-24 [?] CRAN (R 4.4.0)
+# P rsvg           2.6.0    2023-10-08 [?] CRAN (R 4.4.0)
+# P rvest          1.0.4    2024-02-12 [?] CRAN (R 4.4.0)
+# scales       * 1.3.0    2023-11-28 [1] CRAN (R 4.4.0)
+# P selectr        0.4-2    2019-11-20 [?] CRAN (R 4.4.0)
+# P sessioninfo    1.2.2    2021-12-06 [?] CRAN (R 4.4.0)
+# P showtext     * 0.9-7    2024-03-02 [?] CRAN (R 4.4.0)
+# P showtextdb   * 3.0      2020-06-04 [?] CRAN (R 4.4.0)
+# P snakecase      0.11.1   2023-08-27 [?] CRAN (R 4.4.0)
+# P stats        * 4.4.0    2024-04-24 [?] local
+# stringi        1.8.4    2024-05-06 [1] CRAN (R 4.4.0)
+# P stringr      * 1.5.1    2023-11-14 [?] CRAN (R 4.4.0)
+# P svglite        2.1.3    2023-12-08 [?] CRAN (R 4.4.0)
+# P sysfonts     * 0.8.9    2024-03-02 [?] CRAN (R 4.4.0)
+# systemfonts    1.1.0    2024-05-15 [1] CRAN (R 4.4.0)
+# textshaping    0.4.0    2024-05-24 [1] CRAN (R 4.4.0)
+# P tibble       * 3.2.1    2023-03-20 [?] CRAN (R 4.4.0)
+# tidyr        * 1.3.1    2024-01-24 [1] CRAN (R 4.4.0)
+# tidyselect     1.2.1    2024-03-11 [1] CRAN (R 4.4.0)
+# P tidytuesdayR   1.0.3    2023-12-13 [?] CRAN (R 4.4.0)
+# P tidyverse    * 2.0.0    2023-02-22 [?] CRAN (R 4.4.0)
+# P timechange     0.3.0    2024-01-18 [?] CRAN (R 4.4.0)
+# P tools          4.4.0    2024-04-24 [?] local
+# P tzdb           0.4.0    2023-05-12 [?] CRAN (R 4.4.0)
+# P usethis        2.2.3    2024-02-19 [?] CRAN (R 4.4.0)
+# P utf8           1.2.4    2023-10-22 [?] CRAN (R 4.4.0)
+# P utils        * 4.4.0    2024-04-24 [?] local
+# P vctrs          0.6.5    2023-12-01 [?] CRAN (R 4.4.0)
+# P vroom          1.6.5    2023-12-05 [?] CRAN (R 4.4.0)
+# withr          3.0.1    2024-07-31 [1] CRAN (R 4.4.1)
+# P xfun           0.43     2024-03-25 [?] CRAN (R 4.4.0)
+# P xml2           1.3.6    2023-12-04 [?] CRAN (R 4.4.0)
+# 
+# V ── Loaded and on-disk version mismatch.
+# P ── Loaded and on-disk path mismatch.
+# 
+# ───────────────────────────────────────────────────────────────────────────────
+# > 
+#     
